e002bfdcdc5b505c2fc6769a2d9855f9783641d6
mohamed jamoun <mohamed.jamoun@testapp.io>
2021-02-12 15:52:44 +0100

    project base

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyCIzRV8r6NeIhvYWC0Dw_4xNWlJAsxoO2I",
-    authDomain: "jm-company-f7e35.firebaseapp.com",
-    databaseURL: "https://jm-company-f7e35.firebaseio.com",
-    projectId: "jm-company-f7e35",
-    storageBucket: "jm-company-f7e35.appspot.com",
-    messagingSenderId: "973951222937",
-    appId: "1:973951222937:web:99acd22b0cedf834d26408"
-}
